88eedd19d67ceb9759aac2bdca840c01d35c0bfb
Thomas Wirth <thomas@wtho.de>
2021-06-19 13:30:07 +0200

    feat: add semantic commit (dry-run)

---
+++ release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  dryRun: true,
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/github',
+      {
+        assets: 'dist',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        message:
+          'build(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+  branches: [{ name: 'main' }],
+}
